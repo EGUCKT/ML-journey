8d0cfbdc51d3b9152eecf1ed4c17f29e78ba35a3
Atharv Sharma <atharvs278@gmail.com>
2026-06-20 11:35:27 +0530

    DSA patterns of stars, digits and alphabets(halfway through it)

--- practice.py
+++ practice.py
@@ -97,6 +97,8 @@ calculator(a, b)
 print("Thank you for using the calculator!")"""
 
 
+
+"""First unique character"""
 # a = "aiscbgndssydbcyvcdv"
 
 # ans = float('inf')
@@ -112,18 +114,19 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Without switch case for weekdays by nums"""
 # day = int(input("Enter the num: "))
-# wd = ["Monday, Tuesday, wed, thu, fri, sat, sunday"]
-# if day in range(0,8):
+# wd = ["Monday", "Tuesday", "wed", "thu", "fri", "sat", "sunday"]
+
+# if day in range(1, 8):
 #     print(wd[day - 1])
-# else :
+# else:
 #     print("invalid")
 
 
 
 
-
+"""Switch case for days"""
 # match day:
 #         case 1: print("Monday")
 #         case 2: print("Tuesday")
@@ -137,13 +140,13 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Sum of integers from low to high"""
 # low = int(input("enter the low num: "))
 # high= int(input("enter the high num: "))
 
 # count = high - low + 1
     
-     # Apply the mathematical formula using integer division (//)
+#      #Apply the mathematical formula using integer division (//)
 # total_sum = (count * (low + high)) // 2
 
 # print(total_sum)
@@ -152,7 +155,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Sum of first 50 positive integers ending wiht the given digit d"""
 # d = int(input("Enter the num; "))
 
 # count = 0
@@ -170,7 +173,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Reverse the given array"""
 # arr = [1,2,3,3,5,4]
 # n = 6
 # m = 0
@@ -184,16 +187,16 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Square of stars"""
 # for i in range(5):
 #     for j in range(5):
 #         print('*', end=" ")
 #     print()
-    
 
 
 
 
+"""Right angled triangle of stars"""
 # n = 5
 # for i in range(n):
 #     print('*' * (i + 1))
@@ -202,7 +205,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Right angled triangle of digits"""
 # for i in range(6):
 #     for j in range(1, i + 1):
 #         print(j, end=" ")    
@@ -210,7 +213,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Right angled triangle of digits w.r.t. their row number, ex: 1, 22, 333, etc"""
 # for i in range(1, 6):
 #     for j in range(i):
 #         print(i, end=" ")
@@ -220,7 +223,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Reversed right angled triangle of stars"""
 # for i in range(5):
 #     for j in range(5 - i):
 #         print('*', end=" ")
@@ -231,7 +234,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Reversed right angled triangle of digits"""
 # for i in range(5):
 #     for j in range(1, 5 - i + 1):
 #         print(j, end="")
@@ -241,7 +244,7 @@ print("Thank you for using the calculator!")"""
 
 
 
-
+"""Subarray of the maximum total"""
 # s = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 # running_total = s[0]
 # total = s[0]
@@ -249,32 +252,202 @@ print("Thank you for using the calculator!")"""
 # for i in range(1, len(s)):
 #     running_total = max(s[i], running_total + s[i])
 #     total = max(total, running_total)
-#     print(total)
-
-
-# nums = [2, 7, 11, 15]
-# target = 9
-# left = 0
-# right = len(nums) - 1
-# current_sum = nums[left] + nums[right]
-
-# sum = []
-
-# while left < right:
-#     if current_sum == target:
-#         print(left, right)
-#     elif current_sum < target:
-#         left +=1
-#     else:
-#         right -=1
-# print()
+# print(total)
 
 
 
-
+"""Sum of all positive integers from the low num to high num"""
 # sum = 0
 # low = 3
 # high = 8
 # for i in range(low, high + 1):
 #     sum = i + sum
 # print(sum)
+
+
+
+"""Right angled pyramid"""
+# n = 5
+# for i in range(n):
+#     for j in range(1, i+1):
+#         print("*", end=" ")
+#     print()
+
+
+
+"""Right angled reverse pyramid"""
+# n = 5
+# for i in range(n):
+#     for j in range(i, n, +1):
+#         print("*", end=" ")
+#     print()
+
+
+
+
+"""Right angled reverse triangle of digits"""
+# n = 5
+# for i in range(1, n+1):
+#     for j in range(1, (n+1) - i + 1):
+#         print(j, end=" ")
+#     print()
+
+
+
+
+"""Left angled triangle"""
+# n = 10
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end=" ")
+#     for k in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+
+"""Pyramid of stars"""
+# n = 10
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end=" ")
+#     for k in range(2*i+1):
+#         print("*", end=" ")
+#     print()
+
+
+"""Left angled reverse triangle"""
+# n = 10
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(" ", end=" ")
+#     for k in range(n - i - 1):
+#         print("*", end=" ")
+#     print()
+
+
+
+"""Reversed Pyramid of stars"""
+# n = 10
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(" ", end="")
+#     for k in range(2 * n - (2 * i + 1)):
+#         print("*", end="")
+#     print()
+
+
+
+
+
+"""Diamond of stars"""
+# n = 10
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end=" ")
+#     for k in range(2*i+1):
+#         print("*", end=" ")
+#     print()
+# for i in range(n):
+#     for j in range(i):
+#         print(" ", end=" ")
+#     for k in range(2 * n - (2 * i + 1)):
+#         print("*", end=" ")
+#     print()
+
+
+
+"""Right angled diamond"""
+# n = 10
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         print("*", end="")
+#     print()
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print("*", end="")
+#     print()
+
+
+
+"""Binary right angled triangle"""
+"""Bad Practice"""
+# nums = [1, 0]
+# n = 10
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         for u in nums:
+#             print(nums[u], end="")
+#     print()
+
+
+
+"""Best Practice"""
+# n = 10
+
+# for i in range(n):
+#     if i % 2 == 0:
+#         start = 1
+#     else:
+#         start = 0
+#     for j in range(i + 1):
+#         print(start, end="")
+#         start = 1 - start
+#     print()
+
+
+
+
+"""Two right angled pyramids of digits(reversed) together with space in between increasing"""
+# n = 10
+
+# for i in range(1, n):
+#     for j in range(1, i+1):
+#         print(j, end="")
+#     for k in range(2 * n - (2 * i + 2)):
+#         print(" ", end="")
+#     for u in range(i, 0, -1):
+#         print(u, end="")
+#     print()
+
+
+
+"""Print nums for increasing order per row for the number of n rows"""
+# n = 10
+# num = 1
+# for i in range(1, n):
+#     for j in range(1, i + 1):
+#         print(num, end="")
+#         num +=1
+#     print()
+
+
+
+"""Print ABCD for increasing order starting from A for each row"""
+# n = 5
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(chr(65 + j), end="")
+#     print()
+
+
+"""Print ABCD for increasing order on each row with number of repeated A, BB, CCC and so on..."""
+# n = 5
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(chr(65 + i), end="")
+#     print()
+
+
+
+
+"""Print ABCD for decreasing order starting from A for each row n"""
+# n = 10
+# for i in range(n):
+#     for j in range(0, n - (i + 1) + 1):
+#         print(chr(65 + j), end="")
+#     print()
+
+
