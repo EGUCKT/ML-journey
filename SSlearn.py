642b626c61f2af123f72c0560cb6397ff80179ff
Atharv Sharma <atharvs278@gmail.com>
2026-06-27 21:59:36 +0530

    Dsa practice on math

--- SSlearn.py
+++ SSlearn.py
@@ -175,3 +175,51 @@
 #     'crime_reports': [0, 4, 12, 1]              # Incidents reported
 # }
 # df = pd.DataFrame(data)
+
+
+
+"""WORKING ON REAL WORLD DATA FROM MY COLLEGE"""
+import pandas as pd
+import matplotlib as mpl
+import numpy as np
+
+df = pd.read_csv("atsres.csv")
+
+sf = df
+
+"""dealing with str, and symbols like '/', '-', '%' and convertign them to NaN and then filling with median while converting to float"""
+sf['ATS Resume Score'] = sf['ATS Resume Score'].replace(r"[^\d.-]", np.nan, regex=True) 
+
+sf['ATS Resume Score'] = pd.to_numeric(sf['ATS Resume Score'], errors= "coerce")
+
+sf['ATS Resume Score'] = sf['ATS Resume Score'].fillna(sf['ATS Resume Score'].median())
+
+# print('The mean is:')
+# print(sf['ATS Resume Score'].mean())
+
+# print('The lowest score is:')
+# print(sf['ATS Resume Score'].min())
+# print("Whose index is:", sf['ATS Resume Score'].idxmin())
+# print(sf.loc[202])
+
+# print('The highest score is:')
+# print(sf['ATS Resume Score'].max())
+# print("Whose index is:", sf['ATS Resume Score'].idxmax())
+# print(sf.loc[72])
+
+# print(sf.groupby('Section')['ATS Resume Score'].mean())
+
+# print(sf.groupby('Section')['Name'].count())
+
+# print(sf.groupby('Section')['Name'].count().idxmax())
+
+# print(sf.groupby('Section')['Name'].count().idxmin())
+
+# print(sf['ATS Resume Score'].sort_values(ascending = False))
+
+# print(sf.nlargest(10, 'ATS Resume Score'))
+
+# print(sf.groupby('Section')['ATS Resume Score'].idxmax())
+
+# print(sf.groupby('Section')['ATS Resume Score'].max(), sf.groupby('Section')['ATS Resume Score'].min())
+
