c8c60e26307ef36724dfaab9624b6128ccbcc449
Atharv Sharma <atharvs278@gmail.com>
2026-09-03 19:26:35 +0530

    Insertion Sort

--- practice.py
+++ practice.py
@@ -1075,3 +1075,20 @@ print("Thank you for using the calculator!")"""
 # print(nums)
 
 
+"""Insertion Sort"""
+
+# nums = [5, 4, 4, 1, 1]
+
+# for i in range(1, len(nums)):
+#     key = nums[i]
+#     j = i - 1
+    
+#     while j >= 0 and nums[j] > key:
+#         nums[j + 1] = nums[j]
+#         j -= 1
+        
+#     nums[j + 1] = key
+
+# print(nums)
+
+
