25fe991099d176cfcff46a37842d3947555f4f22
Atharv Sharma <atharvs278@gmail.com>
2026-09-04 14:30:13 +0530

    DSA - Merge Sort

--- practice.py
+++ practice.py
@@ -1092,3 +1092,39 @@ print("Thank you for using the calculator!")"""
 # print(nums)
 
 
+"""Merge Sort (Optimized (without extra memory))"""
+# def divide(arr: list[int]) -> list[int]:
+#     if len(arr) <= 1:
+#         return arr
+#     mid = len(arr) // 2
+#     left = arr[:mid]
+#     right = arr[mid:]
+
+#     divide(left)
+#     divide(right)
+
+#     i = 0
+#     j = 0
+#     k = 0
+#     nums = []
+
+#     while i < len(left) and j < len(right):
+#         if left[i] <= right[j]:
+#             arr[k] = left[i]
+#             i += 1
+#         else:
+#             arr[k] = right[j]
+#             j += 1
+#         k += 1
+#     while i < len(left):
+#         arr[k] = left[i]
+#         i += 1
+#         k += 1
+#     while j < len(right):
+#         arr[k] = right[j]
+#         j += 1
+#         k += 1
+#     return arr
+
+# print(divide(arr = [2, 4, 2, 1, 6, 5, 3, 9, 8, 1]))
+
