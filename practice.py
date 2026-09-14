679e882d180aa67bd9344d4d6a493cbdc61828e6
Atharv Sharma <atharvs278@gmail.com>
2026-09-14 14:19:13 +0530

    3 SUM

--- practice.py
+++ practice.py
@@ -1446,3 +1446,29 @@ print("Thank you for using the calculator!")"""
 
 # print(union) 
 
+"""Three sum problem (Leetcode 15)"""
+"""Brute force -> Using nested loops(O(n cube))"""
+"""Using hashmaps -> Extra memory space"""
+"""Most optimal approach -> Two moving pointers + one static pointer"""
+# nums = [-1, 0, 1, 2, -1, -4]
+# result = []
+# nums.sort()
+# for i in range(len(nums)):
+#     if i > 0 and nums[i] == nums[i - 1]:
+#         continue
+#     j = i + 1
+#     k = len(nums) - 1
+#     while j < k:
+#         total = nums[i] + nums[j] + nums[k]
+#         if total > 0:
+#             k -= 1
+#         elif total < 0:
+#             j += 1
+#         else:
+#             result.append([nums[i], nums[j], nums[k]])
+#             j += 1
+#             while nums[j] == nums[j-1] and j < k:
+#                 j += 1
+
+# print(result)
+
